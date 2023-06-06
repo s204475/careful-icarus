de44f1798f8e6f856655e327746fb7c75f0742a5
christian Søtorp <c.soetorp@gmail.com>
2023-06-06 20:10:38 +0200

    added necessary framework to start us off.

---
+++ lib/game/icarus.dart
@@ -0,0 +1,6 @@
+import 'package:flame/game.dart';
+
+class Icarus extends FlameGame {
+  @override
+  Future<void> onLoad() async {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flame/game.dart';
+
+import 'game/icarus.dart';
+import 'game/util/util.dart';
+import 'game/widgets/widgets.dart';
+
+//Main only serves to startup the game, everything else is handled by Icarus
+
+/*    If an errors appears saying something like "start ms-settings:developers"
+*     type win+R and type "ms-settings:developers" and enable developer mode
+*/
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +21,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Icarus',
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        colorScheme: lightColorScheme,
+        useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: darkColorScheme,
+        textTheme: GoogleFonts.audiowideTextTheme(ThemeData.dark().textTheme),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Icarus'),
+    );
+  }
+}
+
+final Game _game = Icarus();
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GameWidget(game: _game),
     );
   }
 }
