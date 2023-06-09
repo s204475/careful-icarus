00bc290cebff4aa08630d3423b2174c13203cff1
RexVerminus <114212231+RexVerminus@users.noreply.github.com>
2023-06-09 11:58:22 +0200

    A bunch of shop_page changes

--- lib/game/widgets/shop_page.dart
+++ lib/game/widgets/shop_page.dart
@@ -7,7 +7,7 @@ class ShopPage extends StatefulWidget {
     const ShopPage (this.game, {super.key});
 
     final Game game;
-
+    
     @override
     State<ShopPage> createState() => _ShopPageState();
 }
@@ -15,6 +15,51 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
     @override
     Widget build(BuildContext context) {
-        return const Text('Shop Page');
+          return Column(
+            children: [
+                  Text('Shop Page'),
+                  Text('[ A string containing how many fish you have :D ]'),
+                  ListView(
+                    padding: const EdgeInsets.all(80),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    children: <Widget>[ //put widget type in here once we have shop buttons
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Hello there!');
+                          },
+                          child: const Text('Say Hello'),
+                        ),
+                        ElevatedButton(
+                        onPressed: () {
+                          print('Hello again!');
+                          },
+                          child: const Text('Say Hello 2'),
+                        ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                        child: ElevatedButton(
+                          onPressed: (){
+                            //set state to game
+                          },
+                          child: const Text('START'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                            textStyle: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        
+                      
+                    ),
+                  )
+            ],
+        );
     }
 }
